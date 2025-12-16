6b2760237b6f6f43e0c6beab8be9366907443611
KARPED1EM <leever.zzz@gmail.com>
2025-12-16 10:14:09 +0800

    fix: update DEFAULT_ASSISTANT_AVATAR path to default image

--- src/core/models/constants.py
+++ src/core/models/constants.py
@@ -4,4 +4,4 @@ Core model-level constants.
 
 DEFAULT_USER_ID = "user"
 DEFAULT_USER_AVATAR = "/static/images/avatar/user.webp"
-DEFAULT_ASSISTANT_AVATAR = "/static/images/avatar/rin.webp"
+DEFAULT_ASSISTANT_AVATAR = "/static/images/avatar/default.webp"
